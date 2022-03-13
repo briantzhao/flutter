7c82fb186484efe99e35521c172eb291e28ccb20
Brian Zhao <bzhao1991@gmail.com>
2022-03-13 19:03:04 -0400

    Adds embedded stream and chat to home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.scss";
-import Header from './components/Header/Header'
-import Home from './pages/Home/Home'
-import Browse from './pages/Browse/Browse'
-import {BrowserRouter as Router, Routes, Route} from react-router-dom
+import Header from "./components/Header/Header";
+import Browse from "./pages/Browse/Browse";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -10,11 +10,11 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path="/following" element={<Browse/>}/>
-          <Route path="/browse" element={<Browse />}/>
-          <Route path="/category/:categoryId" element={<Browse/>}/>
-          <Route path="/channel/:channelId" element={<Home channelId={channelId}/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/following" element={<Browse />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/category/:categoryId" element={<Browse />} />
+          <Route path="/channel/:channelId" element={<Home />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,42 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./Home.scss";
 import axios from "axios";
 
 const API_URL = "https://api.twitch.tv/helix/";
 const token = "jwvny6p6y61zqdkc4c2599537x4rfk";
 
-export default function Home(channelId) {
+export default function Home() {
+  let params = useParams();
+  let channelId = null;
+  if (params.channelId) {
+    channelId = params.channelId;
+  }
   const [featuredVid, setFeaturedVid] = useState(null);
+  const [streams, setStreams] = useState([]);
   useEffect(() => {
     let featured;
-    if (!channelId) {
-      axios.get(`${API_URL}streams`).then(({ data }) => {
-        featured = data[0];
-        data = data.unshift();
-        setStreams(data);
-      });
+    if (channelId === null) {
+      axios
+        .get(`${API_URL}streams?language=en`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Client-Id": `hz0caw5kq9uhz99jiwkj94ixvmg3ej`,
+          },
+        })
+        .then(({ data }) => {
+          console.log(data);
+          data = data.data;
+          for (let i = 0; i < data.length; i++) {
+            console.log(data[i]);
+            if ((data[i].type = "live")) {
+              setFeaturedVid(data[i].user_name);
+              data.splice(i, 1);
+              break;
+            }
+          }
+          setStreams(data);
+        });
     } else {
       axios
         .get(`${API_URL}search/channels?query=${channelId}`, {
@@ -24,21 +46,26 @@ export default function Home(channelId) {
           },
         })
         .then(({ data }) => {
-          featured = data[0];
+          data = data.data;
+          setFeaturedVid(data[0].user_name);
         });
     }
-    setFeaturedVid = `${featured.user_name}&parent=test&muted=true`;
-  });
-  const [streams, setStreams] = useState([]);
+  }, [channelId]);
+
   const [isHomePage, setIsHomePage] = useState(true);
 
   return (
     <div>
       <iframe
-        src={`https://player.twitch.tv/?channel=${featuredVid}`}
-        height="600px"
+        src={`https://player.twitch.tv/?channel=${featuredVid}&parent=localhost&muted=true`}
+        height="450px"
         width="800px"
-        allowfullscreen="true"
+        allowFullScreen={true}
+      ></iframe>
+      <iframe
+        src={`https://www.twitch.tv/embed/${featuredVid}/chat?parent=localhost`}
+        height="600px"
+        width="400px"
       ></iframe>
     </div>
   );
