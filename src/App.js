2ea6b1a81df7dfd99b518cfd972b6c2a0d5de17c
Brian Zhao <bzhao1991@gmail.com>
2022-03-13 16:58:18 -0400

    Installs sass and react-router-dom, adds routes to App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import {BrowserRouter as Router, Routes, Route} from react-router-dom
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path="/following" element={<Favorites/>}/>
+          <Route path="/browse" element={<Browse />}/>
+          <Route path="/category/:categoryId" element={<Browse/>}/>
+          <Route path="/channel/:channelId" element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
